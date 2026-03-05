43fbea324669c4e7bd35819951cc16a04e4bbbfb
v0id <>
2026-03-05 12:53:59 +0200

    chore(config): add SERP API config

---
+++ config/serp.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'base_url' => env('SERP_API_BASE_URL', 'https://api.dataforseo.com/v3'),
+
+    'login' => env('SERP_API_LOGIN'),
+    'password' => env('SERP_API_PASSWORD'),
+
+    'timeout' => (int)env('SERP_API_TIMEOUT', 90),
+    'connect_timeout' => (int)env('SERP_API_CONNECT_TIMEOUT', 10),
+
+    'retry' => [
+        'times' => (int)env('SERP_API_RETRY_TIMES', 2),
+        'sleep_ms' => (int)env('SERP_API_RETRY_SLEEP_MS', 250),
+    ],
+
+    'google' => [
+        'device' => env('SERP_DEVICE', 'desktop'),
+        'depth' => (int)env('SERP_DEPTH', 100),
+
+        // Optional: domain override
+        'se_domain' => env('SERP_SE_DOMAIN'),
+    ],
+];
